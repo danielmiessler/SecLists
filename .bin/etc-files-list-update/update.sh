508ba2b0a80f4f68b0395a685e858faa10b01599
ItsIgnacioPortal <46269721+ItsIgnacioPortal@users.noreply.github.com>
2026-03-06 20:55:40 -0300

    fix(cicd): Updated Linux LFI etc-files wordlist path

--- .bin/etc-files-list-update/update.sh
+++ .bin/etc-files-list-update/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export listpath="../../Fuzzing/LFI/LFI-etc-files-of-all-linux-packages.txt"
+export listpath="../../Fuzzing/LFI/Linux/LFI-etc-files-of-all-linux-packages.txt"
 [ -f all_files.gz ] && rm all_files.gz
 
 # every year, start a new gzip list so there is not as much bloat updating blobs in git
